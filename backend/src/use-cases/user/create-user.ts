9711357c980bfb6eb092197d138e6c2b26521d52
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-03 17:50:23 -0300

    feat: add CreateUserUseCase

---
+++ backend/src/use-cases/user/create-user.ts
@@ -0,0 +1,28 @@
+import bcrypt from 'bcryptjs';
+import type { IUser } from '../../@types/IUser';
+import { v4 as uuid } from 'uuid';
+
+interface ICreateUserRepository {
+  execute(user: IUser): Promise<IUser>;
+}
+
+export class CreateUserUseCase {
+  private createUserRepository: ICreateUserRepository;
+  constructor(createUserRepository: ICreateUserRepository) {
+    this.createUserRepository = createUserRepository;
+  }
+
+  async execute(user: IUser) {
+    const userId = uuid();
+
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const createUser = await this.createUserRepository.execute({
+      ...user,
+      password: hashedPassword,
+      id: userId,
+    });
+
+    return createUser;
+  }
+}

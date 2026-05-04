ff0dde59a501447c0a874d48a6eba0251993d983
Bruno <Bruno2.jean.lima@outlook.com>
2026-05-04 13:21:45 -0300

    feat: add router DeleteUser

--- api/src/router/user.ts
+++ api/src/router/user.ts
@@ -1,11 +1,25 @@
 import { Router, type IRouter, type Request, type Response } from 'express';
-import { makeCreateUseController } from '../factories/controllers';
+import {
+  makeCreateUseController,
+  makeDeleteUserController,
+} from '../factories/controllers';
+import { auth } from '../middlewares/auth';
 
 const userRoutes: IRouter = Router();
 
 userRoutes.post('/', async (request: Request, response: Response) => {
   const createUserController = makeCreateUseController();
   const { statusCode, body } = await createUserController.execute(request);
+
+  return response.status(statusCode).send(body);
+});
+
+userRoutes.delete('/me', auth, async (request: Request, response: Response) => {
+  const deleteUserController = makeDeleteUserController();
+
+  request.params.userId = request.userId as string;
+
+  const { statusCode, body } = await deleteUserController.execute(request);
 
   return response.status(statusCode).send(body);
 });

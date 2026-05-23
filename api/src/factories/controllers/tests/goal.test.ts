52301690cc4f9a5930d3f98317fa0d2d3875a9e9
Bruno <Bruno2.jean.lima@outlook.com>
2026-05-23 13:11:11 -0300

    test: ensure GetGoalsController test

--- api/src/factories/controllers/tests/goal.test.ts
+++ api/src/factories/controllers/tests/goal.test.ts
@@ -1,9 +1,27 @@
-import { CreateGoalController } from '../../../controllers';
-import { makeCreateGoalController } from '../goal';
+import {
+  CreateGoalController,
+  GetGoalByIdController,
+  GetGoalsController,
+} from '../../../controllers';
+import {
+  makeCreateGoalController,
+  makeGetGoalByIdController,
+  makeGetGoalsController,
+} from '../goal';
 
 describe('Factory Goal Controller', () => {
   test('should return a CreateGoalController', () => {
     const createGoalController = makeCreateGoalController();
     expect(createGoalController).toBeInstanceOf(CreateGoalController);
+  });
+
+  test('should return a GetGoalsController', () => {
+    const getGoalsController = makeGetGoalsController();
+    expect(getGoalsController).toBeInstanceOf(GetGoalsController);
+  });
+
+  test('should return a GetGoalByIdController', () => {
+    const getGoalByIdController = makeGetGoalByIdController();
+    expect(getGoalByIdController).toBeInstanceOf(GetGoalByIdController);
   });
 });

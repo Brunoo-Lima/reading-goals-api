a8c853fa81490ca5ad7250f395068377f67b9671
Bruno <Bruno2.jean.lima@outlook.com>
2026-05-23 12:42:58 -0300

    fix: the route for creating the meta tag has been changed to pass "auth" and the request.body has been changed to request.params

--- api/src/router/goal.ts
+++ api/src/router/goal.ts
@@ -1,12 +1,13 @@
 import { Router, type IRouter, type Request, type Response } from 'express';
-import { makeCreateBookController } from '../factories/controllers';
+import { makeCreateGoalController } from '../factories/controllers';
+import { auth } from '../middlewares/auth';
 
 const goalRoutes: IRouter = Router();
 
-goalRoutes.post('/', async (request: Request, response: Response) => {
-  const createGoalController = makeCreateBookController();
+goalRoutes.post('/', auth, async (request: Request, response: Response) => {
+  const createGoalController = makeCreateGoalController();
 
-  request.body.user_id = request.userId as string;
+  request.params.userId = request.userId as string;
 
   const { statusCode, body } = await createGoalController.execute(request);
 

f2e133a3da5aa865bcdfc06679c005e0464c8400
Bruno <Bruno2.jean.lima@outlook.com>
2026-05-26 20:38:39 -0300

    feat: add CreateNoteUseCase

--- api/src/interfaces/use-cases/index.ts
+++ api/src/interfaces/use-cases/index.ts
@@ -3,3 +3,4 @@ export * from './user';
 export * from './auth';
 export * from './reading-log';
 export * from './goal';
+export * from './note';
---
+++ api/src/use-cases/note/create-note.ts
@@ -0,0 +1,41 @@
+import type { INote } from '../../@types/INote';
+import { BookNotFoundError } from '../../errors';
+import type { IIdGeneratorAdapter } from '../../interfaces/adapters';
+import type {
+  ICreateNoteRepository,
+  IGetBookByIdRepository,
+} from '../../interfaces/repositories';
+
+export class CreateNoteUseCase {
+  private createNoteRepository: ICreateNoteRepository;
+  private getBookByIdRepository: IGetBookByIdRepository;
+  private idGeneratorAdapter: IIdGeneratorAdapter;
+
+  constructor(
+    createNoteRepository: ICreateNoteRepository,
+    getBookByIdRepository: IGetBookByIdRepository,
+    idGeneratorAdapter: IIdGeneratorAdapter,
+  ) {
+    this.createNoteRepository = createNoteRepository;
+    this.getBookByIdRepository = getBookByIdRepository;
+    this.idGeneratorAdapter = idGeneratorAdapter;
+  }
+
+  async execute(noteParams: INote, bookId: string) {
+    const book = await this.getBookByIdRepository.execute(bookId);
+
+    if (!book) {
+      throw new BookNotFoundError();
+    }
+
+    const noteId = this.idGeneratorAdapter.execute();
+
+    const note = {
+      ...noteParams,
+      id: noteId,
+      book_id: book.id,
+    };
+
+    return await this.createNoteRepository.execute(note);
+  }
+}

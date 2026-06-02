6fe385545a2c568cf0a677bf1e64ebfc18211236
Bruno <Bruno2.jean.lima@outlook.com>
2026-06-02 15:15:18 -0300

    feat: add query bookId to retrieve the notes for a specific book GetNotesByUserIdController

--- api/src/controllers/note/get-notes-by-user-id.ts
+++ api/src/controllers/note/get-notes-by-user-id.ts
@@ -1,13 +1,14 @@
 import type { Request } from 'express';
 import type { IGetNotesByUserIdUseCase } from '../../interfaces/use-cases';
 import {
+  bookNotFoundResponse,
   checkIfIdIsValid,
   invalidIdResponse,
   ok,
   serverError,
   userNotFoundResponse,
 } from '../helpers';
-import { UserNotFoundError } from '../../errors';
+import { BookNotFoundError, UserNotFoundError } from '../../errors';
 
 export class GetNotesByUserIdController {
   private getNotesByUserIdUseCase: IGetNotesByUserIdUseCase;
@@ -19,19 +20,25 @@ export class GetNotesByUserIdController {
   async execute(request: Request) {
     try {
       const userId = request.params.userId as string;
+      const bookId = request.query.bookId as string;
 
       const isIdValid = checkIfIdIsValid(userId);
+      const isBookIdValid = checkIfIdIsValid(bookId);
 
-      if (!isIdValid) {
+      if (!isIdValid || !isBookIdValid) {
         return invalidIdResponse();
       }
 
-      const notes = await this.getNotesByUserIdUseCase.execute(userId);
+      const notes = await this.getNotesByUserIdUseCase.execute(userId, bookId);
 
       return ok(notes);
     } catch (error) {
       if (error instanceof UserNotFoundError) {
         return userNotFoundResponse();
+      }
+
+      if (error instanceof BookNotFoundError) {
+        return bookNotFoundResponse();
       }
 
       return serverError();

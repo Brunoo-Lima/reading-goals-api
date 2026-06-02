8877d5d66ec749567edbf8d131165ef49d7b9859
Bruno <Bruno2.jean.lima@outlook.com>
2026-06-02 10:54:26 -0300

    feat: add notes on fixtures test

--- api/src/tests/fixtures/note.ts
+++ api/src/tests/fixtures/note.ts
@@ -10,3 +10,16 @@ export const note = {
   created_at: faker.date.anytime(),
   updated_at: faker.date.anytime(),
 };
+
+export const notes = [
+  {
+    id: faker.string.uuid(),
+    content: faker.lorem.sentence(),
+    rating: faker.number.int({ min: 1, max: 5 }),
+    page_number: 300,
+    user_id: faker.string.uuid(),
+    book_id: faker.string.uuid(),
+    created_at: faker.date.anytime(),
+    updated_at: faker.date.anytime(),
+  },
+];

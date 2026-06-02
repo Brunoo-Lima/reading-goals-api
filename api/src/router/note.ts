14722917ec9c1d93c9b4b7114ec39bcafe78b3bb
Bruno <Bruno2.jean.lima@outlook.com>
2026-06-02 11:00:04 -0300

    feat: add get router GetNotesByUserId

--- api/src/router/note.ts
+++ api/src/router/note.ts
@@ -1,6 +1,9 @@
 import { Router, type IRouter, type Request, type Response } from 'express';
 import { auth } from '../middlewares/auth';
-import { makeCreateNoteController } from '../factories/controllers';
+import {
+  makeCreateNoteController,
+  makeGetNotesByUserIdController,
+} from '../factories/controllers';
 
 const noteRoutes: IRouter = Router();
 
@@ -17,5 +20,16 @@ noteRoutes.post(
     return response.status(statusCode).send(body);
   },
 );
+
+noteRoutes.get('/', auth, async (request: Request, response: Response) => {
+  const getNotesByUserIdController = makeGetNotesByUserIdController();
+
+  request.params.userId = request.userId as string;
+
+  const { statusCode, body } =
+    await getNotesByUserIdController.execute(request);
+
+  return response.status(statusCode).send(body);
+});
 
 export { noteRoutes };

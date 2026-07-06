33d580111a948f79feb9a060480f7a3dbe7ff71c
Bruno <Bruno2.jean.lima@outlook.com>
2026-07-06 14:15:12 -0300

    feat: add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,10 @@
 import express, { type Application } from 'express';
+import cors from 'cors';
 import { routes } from './router';
 
 const app: Application = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 export { app };

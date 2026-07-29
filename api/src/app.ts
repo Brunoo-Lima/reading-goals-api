b1b0d67ba1336a0fd1833c6eaf078a15970de904
Bruno <Bruno2.jean.lima@outlook.com>
2026-07-29 14:48:36 -0300

    feat: add cookieParser

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,17 @@
 import express, { type Application } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { routes } from './router';
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // URL exata front
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 app.use(routes);
 
 export { app };

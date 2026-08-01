f5bdd230d3ae087ffe0e4ada55a2ab8198d24e1b
Bruno <Bruno2.jean.lima@outlook.com>
2026-08-01 17:23:34 -0300

    feat: add ForgotPasswordController

---
+++ api/src/controllers/auth/forgot-password.ts
@@ -0,0 +1,44 @@
+import { type Request } from 'express';
+import { badRequest, ok, serverError } from '../helpers';
+import type { IForgotPasswordUseCase } from '../../interfaces/use-cases';
+import { forgotPasswordSchema } from '../../schemas';
+import { ZodError } from 'zod';
+import { DataInvalidError } from '../../errors';
+
+export class ForgotPasswordController {
+  private forgotPasswordUseCase: IForgotPasswordUseCase;
+
+  constructor(forgotPasswordUseCase: IForgotPasswordUseCase) {
+    this.forgotPasswordUseCase = forgotPasswordUseCase;
+  }
+
+  async execute(request: Request) {
+    try {
+      const params = request.body;
+
+      await forgotPasswordSchema.parseAsync(params);
+
+      await this.forgotPasswordUseCase.execute(
+        params.email,
+        params.securityKey,
+      );
+
+      return ok({
+        message:
+          'Se o e-mail existir e a chave de segurança estiver correta você será redirecionado para a página de redefinição de senha. Aguarde...',
+      });
+    } catch (error) {
+      console.error(error);
+
+      if (error instanceof ZodError) {
+        return badRequest({ message: error.issues[0]?.message });
+      }
+
+      if (error instanceof DataInvalidError) {
+        return badRequest({ message: error.message });
+      }
+
+      return serverError();
+    }
+  }
+}

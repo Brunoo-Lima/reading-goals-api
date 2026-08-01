fda170dafe618e061ec100062cb1e4b928c1b6d3
Bruno <Bruno2.jean.lima@outlook.com>
2026-07-31 22:03:34 -0300

    feat: add service update user

--- web/src/services/user.ts
+++ web/src/services/user.ts
@@ -1,29 +1,15 @@
 import { useMutation } from '@tanstack/react-query';
 import api from './api';
-import type { IUser } from '@/@types/IUser';
+import type { ICreateUser, IUpdateUser } from '@/@types/IUser';
 import type { AxiosError } from 'axios';
 import { toast } from 'sonner';
 
 export const getUser = async () => {
-  const { data } = await api.get('/users/me', {
-    // params: {
-    //   userId,
-    // },
-  });
-
+  const { data } = await api.get('/users/me');
   return data;
 };
 
-// export const useGetUser = () => {
-//   return useMutation({
-//     mutationKey: ['getUser'],
-//     mutationFn: (userId: string) => {
-//       return getUser(userId);
-//     },
-//   });
-// };
-
-export const createUser = async (user: IUser) => {
+export const createUser = async (user: ICreateUser) => {
   const { data } = await api.post('/users', user);
   return data;
 };
@@ -31,19 +17,35 @@ export const createUser = async (user: IUser) => {
 export const useCreateUser = () => {
   return useMutation({
     mutationKey: ['signup'],
-    mutationFn: (user: IUser) => {
+    mutationFn: (user: ICreateUser) => {
       return createUser(user);
     },
-    onSuccess: (createdUser) => {
-      const { accessToken, refreshToken } = createdUser.tokens;
-
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('refreshToken', refreshToken);
-
+    onSuccess: () => {
       toast.success('Usuário criado com sucesso!');
     },
     onError: (error: AxiosError) => {
       const message = error.message || 'Erro ao criar usuário.';
+      toast.error(message);
+    },
+  });
+};
+
+export const updateUser = async (user: IUpdateUser) => {
+  const { data } = await api.patch('/users/me', user);
+  return data;
+};
+
+export const useUpdateUser = () => {
+  return useMutation({
+    mutationKey: ['updateUser'],
+    mutationFn: (user: IUpdateUser) => {
+      return updateUser(user);
+    },
+    onSuccess: () => {
+      toast.success('Usuário atualizado com sucesso!');
+    },
+    onError: (error: AxiosError) => {
+      const message = error.message || 'Erro ao atualizar usuário.';
       toast.error(message);
     },
   });

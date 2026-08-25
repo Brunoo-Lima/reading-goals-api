59b0b1484e5efc3ed61f78baf1e92ee3022e6deb
Bruno <Bruno2.jean.lima@outlook.com>
2026-08-25 09:16:08 -0300

    test: ensure ForgotPasswordController

---
+++ api/src/controllers/auth/tests/forgot-password.test.ts
@@ -0,0 +1,66 @@
+import type { Request } from 'express';
+import { ForgotPasswordController } from '../forgot-password';
+
+describe('Forgot Password Controller', () => {
+  class ForgotPasswordUseCaseStub {
+    async execute() {
+      return;
+    }
+  }
+
+  const makeSut = () => {
+    const forgotPasswordUseCaseStub = new ForgotPasswordUseCaseStub();
+    const sut = new ForgotPasswordController(forgotPasswordUseCaseStub);
+
+    return { sut, forgotPasswordUseCaseStub };
+  };
+
+  const baseHttpRequest = {
+    body: {
+      email: 'test@test.com',
+    },
+  } as Partial<Request> as Request;
+
+  it('should return 200 on forgot password success', async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute(baseHttpRequest);
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should return 400 if email is missing', async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      body: {
+        email: undefined,
+      },
+    } as Partial<Request> as Request);
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('should return 400 if email is invalid', async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      body: {
+        email: 'invalid-email',
+      },
+    } as Partial<Request> as Request);
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('should return 500 if ForgotPasswordUseCase throws', async () => {
+    const { sut, forgotPasswordUseCaseStub } = makeSut();
+    vi.spyOn(forgotPasswordUseCaseStub, 'execute').mockRejectedValueOnce(
+      new Error(),
+    );
+
+    const response = await sut.execute(baseHttpRequest);
+
+    expect(response.statusCode).toBe(500);
+  });
+});

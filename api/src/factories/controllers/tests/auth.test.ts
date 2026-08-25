c8fcfec30ad1b41b9e4a1cd455a11fe920fcf7fa
Bruno <Bruno2.jean.lima@outlook.com>
2026-08-25 09:16:40 -0300

    test: ensure ResetPasswordController and ForgotPasswordController factories

--- api/src/factories/controllers/tests/auth.test.ts
+++ api/src/factories/controllers/tests/auth.test.ts
@@ -1,5 +1,16 @@
-import { LoginController, RefreshTokenController } from '../../../controllers';
-import { makeLoginController, makeRefreshTokenController } from '../auth';
+import {
+  ForgotPasswordController,
+  LoginController,
+  RefreshTokenController,
+  ResetPasswordController,
+} from '../../../controllers';
+
+import {
+  makeForgotPasswordController,
+  makeLoginController,
+  makeRefreshTokenController,
+  makeResetPasswordController,
+} from '../auth';
 
 describe('Factory Auth Controller', () => {
   test('should return a LoginController', () => {
@@ -10,5 +21,15 @@ describe('Factory Auth Controller', () => {
   test('should return a RefreshTokenController', () => {
     const refreshTokenController = makeRefreshTokenController();
     expect(refreshTokenController).toBeInstanceOf(RefreshTokenController);
+  });
+
+  test('should return a ForgotPasswordController', () => {
+    const forgotPasswordController = makeForgotPasswordController();
+    expect(forgotPasswordController).toBeInstanceOf(ForgotPasswordController);
+  });
+
+  test('should return a ResetPasswordController', () => {
+    const resetPasswordController = makeResetPasswordController();
+    expect(resetPasswordController).toBeInstanceOf(ResetPasswordController);
   });
 });
